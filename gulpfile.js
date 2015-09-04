5049e5682bf71c44d8f9707fbfd4fbc032f6b15d
Dave Lockhart <dave.lockhart@d2l.com>
2015-09-04 18:18:58 -0400

    pulling icons and change-tracking from Bower

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,28 @@ var opts = {
 	version: process.env.TRAVIS_TAG
 };
 
-var gulp = require('gulp'),
+var concat = require('gulp-concat'),
+	gulp = require('gulp'),
 	pg = require('peanut-gallery'),
 	publisher = require('gulp-frau-publisher').lib(opts);
+
+var jsFiles = [
+		/*'bower_components/vui-accordion-jquery/accordion.js',*/
+		'bower_components/vui-change-tracking-jquery/changeTracker.js',
+		'bower_components/vui-change-tracking-jquery/changeTracking.js',
+		/*'bower_components/vui-collapsible-section-jquery/collapsibleSection.js',*/
+		'bower_components/jquery-vui-more-less/moreLess.js'/*,
+		'bower_components/vui-scrollspy-jquery/scroll-spy.js'*/
+	];
+
+gulp.task('javascript', function() {
+	return gulp.src( jsFiles )
+		.pipe( concat('bsi.js') )
+		.pipe( gulp.dest('./dist') )
+		/*.pipe( jsmin() )
+		.pipe( rename( { suffix: '.min' } ) )*/
+		.pipe( gulp.dest('./dist') );
+});
 
 gulp.task('publish', function(cb) {
 	gulp.src('./dist/**')
--- src/index.js
+++
@@ -1,1 +0,0 @@
-import moreLess from '../bower_components/jquery-vui-more-less/moreLess.js';
