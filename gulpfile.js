17ae7fa538dfea31c063aecf7d6260a7fca3697b
Dave Lockhart <dave.lockhart@d2l.com>
2015-09-08 15:36:00 -0400

    CLI publisher and minifying

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
 'use strict';
 
-var opts = {
-	targetDirectory: 'bsi',
-	creds: {
-		key: 'AKIAIZGPQIRSA2WEJEDA',
-		secret: process.env.CDN_SECRET
-	},
-	version: process.env.TRAVIS_TAG
-};
-
 var concat = require('gulp-concat'),
-	gulp = require('gulp'),
-	pg = require('peanut-gallery'),
-	publisher = require('gulp-frau-publisher').lib(opts);
+	gulp = require('gulp');
 
 var jsFiles = [
 		'bower_components/jquery-vui-accordion/accordion.js',
@@ -30,18 +19,4 @@ gulp.task('javascript', function() {
 		/*.pipe( jsmin() )
 		.pipe( rename( { suffix: '.min' } ) )*/
 		.pipe( gulp.dest('./dist') );
-});
-
-gulp.task('publish', function(cb) {
-	gulp.src('./dist/**')
-		.pipe(publisher.getStream())
-		.on('end', function() {
-			var message = '[Deployment available online](' +
-				publisher.getLocation() + ')';
-			pg.comment(message, {}, function(error, response) {
-				if(error)
-					return cb(JSON.stringify(error));
-				cb();
-			} );
-		} );
 });
