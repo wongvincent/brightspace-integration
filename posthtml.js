c36ec6d953edd60564b80936b1e11d108f0dd661
Dave Lockhart <dave.lockhart@d2l.com>
2016-07-11 14:19:35 -0400

    minifying HTML files as part of build

---
+++ posthtml.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+	minifier: {
+		collapseWhitespace: true,
+		ignoreCustomComments: [/^\s@license/],
+		minifyCSS: true,
+		minifyJS: true,
+		removeComments: true
+	}
+};
