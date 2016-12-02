6c63e0c00c0a0b76311f45bc6b26a892b775de3c
Dave Lockhart <dave.lockhart@d2l.com>
2016-12-02 12:48:31 -0500

    logging measures as part of load event

--- src/page-loading/page-loading.js
+++ src/page-loading/page-loading.js
@@ -1,51 +1,27 @@
 (function() {
 	'use strict';
 
-	var pageReady, pageLoaded = false;
+	var pageReady = false;
 
-	function logMeasures() {
-
-		if (!pageReady || !pageLoaded) {
+	function pageIsReady() {
+		if (pageReady) {
 			return;
 		}
+		pageReady = true;
+		D2L.Performance.measure('d2l.page.display', 'fetchStart');
+		document.body.classList.remove('d2l-page-loading');
+	}
 
+	addEventListener('load', function() {
 		D2L.Performance.measure('d2l.page.preFetch', 'navigationStart', 'fetchStart');
 		D2L.Performance.measure('d2l.page.domInteractive', 'fetchStart', 'domInteractive');
 		D2L.Performance.measure('d2l.page.domContentLoadedHandlers', 'domContentLoadedEventStart', 'domContentLoadedEventEnd');
 		D2L.Performance.measure('d2l.page.load', 'fetchStart', 'loadEventStart');
-
-	}
-
-	function check() {
-		if (!pageReady) {
-			return;
-		}
-		D2L.Performance.measure('d2l.page.display', 'fetchStart');
-		document.body.classList.remove('d2l-page-loading');
-		logMeasures();
-	}
-
-	function pageIsReady() {
-
-		if (pageReady) {
-			return;
-		}
-
-		pageReady = true;
-		check();
-
-	}
-
-	addEventListener('load', function() {
-		pageLoaded = true;
-		logMeasures();
 	});
 
+	// polyfill in use
 	if (window.WebComponents) {
-		addEventListener('WebComponentsReady', function() {
-			D2L.Performance.measure('d2l.page.webComponentsReady', 'fetchStart');
-			pageIsReady();
-		});
+		addEventListener('WebComponentsReady', pageIsReady);
 	} else {
 		if (document.readyState === 'interactive' || document.readyState === 'complete') {
 			pageIsReady();
