d1456a11414716b5a53667c10f9a5313626c92b2
Dave Lockhart <dave.lockhart@d2l.com>
2016-12-02 10:35:40 -0500

    renaming domContentLoaded measure to more accurately reflect its purpose

--- src/page-loading/page-loading.js
+++ src/page-loading/page-loading.js
@@ -11,7 +11,7 @@
 
 		D2L.Performance.measure('d2l.page.preFetch', 'navigationStart', 'fetchStart');
 		D2L.Performance.measure('d2l.page.domInteractive', 'fetchStart', 'domInteractive');
-		D2L.Performance.measure('d2l.page.domContentLoaded', 'domContentLoadedEventStart', 'domContentLoadedEventEnd');
+		D2L.Performance.measure('d2l.page.domContentLoadedHandlers', 'domContentLoadedEventStart', 'domContentLoadedEventEnd');
 		D2L.Performance.measure('d2l.page.load', 'fetchStart', 'loadEventStart');
 
 	}
