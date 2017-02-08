d16f4b8b447ed00d12beb7ca444e5f4c279566bd
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2017-02-08 18:11:13 -0500

    Add d2l-telemetry

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ require('../bower_components/jquery-vui-change-tracking/changeTracking.js');
 require('../bower_components/jquery-vui-collapsible-section/collapsibleSection.js');
 require('../bower_components/jquery-vui-more-less/moreLess.js');
 require('../bower_components/jquery-vui-scrollspy/scroll-spy.js');
+require('../bower_components/d2l-telemetry/d2l-telemetry.js');
 
 window.BSI = window.BSI || {};
 window.BSI.Intl = require('d2l-intl');
