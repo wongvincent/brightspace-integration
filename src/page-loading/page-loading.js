0f21206b1cdd8fb4256c62ccb1d27e29f6408774
Dave Lockhart <dave.lockhart@d2l.com>
2017-03-01 17:46:43 -0500

    adding timings for time spent on the server, downloading and from download end to web-component-ready

--- src/page-loading/page-loading.js
+++ src/page-loading/page-loading.js
@@ -6,6 +6,8 @@
 		D2L.Performance.measure('d2l.page.domInteractive', 'fetchStart', 'domInteractive');
 		D2L.Performance.measure('d2l.page.domContentLoadedHandlers', 'domContentLoadedEventStart', 'domContentLoadedEventEnd');
 		D2L.Performance.measure('d2l.page.load', 'fetchStart', 'loadEventStart');
+		D2L.Performance.measure('d2l.page.server', 'requestStart', 'responseStart');
+		D2L.Performance.measure('d2l.page.download', 'responseStart', 'responseEnd');
 	});
 
 })();
