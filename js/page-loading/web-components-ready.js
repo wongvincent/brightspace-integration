16122e0039cacafddf3c7146c4a74abb64ddc99b
Dave Lockhart <dave.lockhart@d2l.com>
2017-10-05 14:16:32 -0400

    using native performance APIs instead of polyfill

--- js/page-loading/web-components-ready.js
+++ js/page-loading/web-components-ready.js
@@ -3,16 +3,48 @@
 
 	var pageReady = false;
 
+	function measure(name, startMark, endMark) {
+		if (!window.performance
+			|| !window.performance.mark
+			|| !window.performance.measure
+			|| !window.performance.getEntriesByName) return;
+		if (!endMark) {
+			window.performance.mark(name);
+			endMark = name;
+		}
+		window.performance.measure(name, startMark, endMark);
+		// following should be replaced with PerformanceObserver when IE/Edge support it
+		var measure = window.performance.getEntriesByName(name, 'measure');
+		if (measure.length === 1 ) {
+			document.dispatchEvent(new CustomEvent('D2LPerformanceMeasure', {
+				bubbles: true,
+				detail: { name: name, value: measure[0] }
+			}));
+		}
+	}
+
 	function pageIsReady() {
 		if (pageReady) {
 			return;
 		}
 		pageReady = true;
-		// on slow CPUs, this can actually fire before the telemetry code has initialized
-		if (D2L && D2L.Performance) {
-			D2L.Performance.measure('d2l.page.visible', 'responseEnd');
-			D2L.Performance.measure('d2l.page.display', 'fetchStart');
-		}
+		measure('d2l.page.visible', 'responseEnd');
+		measure('d2l.page.display', 'fetchStart');
+	}
+
+	function measurePageEvents() {
+		measure('d2l.page.preFetch', 'navigationStart', 'fetchStart');
+		measure('d2l.page.domInteractive', 'fetchStart', 'domInteractive');
+		measure('d2l.page.domContentLoadedHandlers', 'domContentLoadedEventStart', 'domContentLoadedEventEnd');
+		measure('d2l.page.load', 'fetchStart', 'loadEventStart');
+		measure('d2l.page.server', 'requestStart', 'responseStart');
+		measure('d2l.page.download', 'responseStart', 'responseEnd');
+	}
+
+	if (document.readyState === 'complete') {
+		measurePageEvents();
+	} else {
+		addEventListener('load', measurePageEvents);
 	}
 
 	// polyfill in use
