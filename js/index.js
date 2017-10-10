1a640c2637694ae78ff7765424293427b58f2d1a
Ryan Mahler <ryan.mahler@d2l.com>
2017-10-10 15:33:51 -0400

    Add Array.prototype.includes polyfill for d2l-fetch-simple-cache
    Not currently an issue due to how things are structured, but I forgot to move this polyfill over from d2l-my-courses when I restructured things. Required for IE11 because of course it is.

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ require('../bower_components/jquery-vui-more-less/moreLess.js');
 require('../bower_components/jquery-vui-scrollspy/scroll-spy.js');
 require('../bower_components/d2l-telemetry/d2l-telemetry.js');
 require('./page-loading/timing-debug.js');
+require('./polyfill/Array.prototype.includes.js');
 
 window.BSI = window.BSI || {};
 window.BSI.Intl = require('d2l-intl');
