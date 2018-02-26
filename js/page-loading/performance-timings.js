7e598841ef8bc262c2b3b8f7c1be233afed304d9
Dave Lockhart <dave.lockhart@d2l.com>
2018-02-26 17:39:31 -0500

    adjusting timing debug overlay to handle any number of events

--- js/page-loading/performance-timings.js
+++ js/page-loading/performance-timings.js
@@ -15,7 +15,7 @@
 		// following should be replaced with PerformanceObserver when IE/Edge support it
 		var measure = window.performance.getEntriesByName(name, 'measure');
 		if (measure.length === 1 && fireEvent) {
-			document.dispatchEvent(new CustomEvent('D2LPerformanceMeasure', {
+			document.dispatchEvent(new CustomEvent('d2l-performance-measure', {
 				bubbles: true,
 				detail: { name: name, value: measure[0] }
 			}));
@@ -36,13 +36,13 @@
 	function tryMeasure() {
 		if (!wcReadyFired || !pageLoadedFired || measured) return;
 		measured = true;
-		measure('d2l.page-init.preFetch', 'navigationStart', 'fetchStart', false);
-		measure('d2l.page-init.domInteractive', 'fetchStart', 'domInteractive', false);
-		measure('d2l.page-init.domContentLoadedHandlers', 'domContentLoadedEventStart', 'domContentLoadedEventEnd', false);
-		measure('d2l.page-init.load', 'fetchStart', 'loadEventStart', false);
-		measure('d2l.page-init.server', 'requestStart', 'responseStart', false);
-		measure('d2l.page-init.download', 'responseStart', 'responseEnd', false);
-		measure('d2l.page-init.webComponentsReady', 'responseEnd', 'webComponentsReady', true);
+		measure('d2l.page.preFetch', 'navigationStart', 'fetchStart', false);
+		measure('d2l.page.domInteractive', 'fetchStart', 'domInteractive', false);
+		measure('d2l.page.domContentLoadedHandlers', 'domContentLoadedEventStart', 'domContentLoadedEventEnd', false);
+		measure('d2l.page.load', 'fetchStart', 'loadEventStart', false);
+		measure('d2l.page.server', 'requestStart', 'responseStart', false);
+		measure('d2l.page.download', 'responseStart', 'responseEnd', false);
+		measure('d2l.page.webComponentsReady', 'responseEnd', 'webComponentsReady', true);
 	}
 
 	if (document.readyState === 'complete') {
